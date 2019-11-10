ff7ee507e87085bbe5299d7a415ce376a99fa2b5
Nikhil <nykhilamatya@gmail.com>
2019-11-10 19:02:11 +1100

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -15,7 +16,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "User details were updated"
+      redirect_to profiles_path
+    else
+      render 'edit'
+    end
+  end
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
